4db453842f68bfffff7f6d4e0fb73443629b096e
DeepSleepEngineer <deep-sleeper@nify.com>
2018-01-02 17:19:52 +0900

    Update sequence -Change sequence to run test 10 times in one sequence. -Add output array of pointer to test file names. -Update binary of application in EV3.

--- MotorSpecTest/app.c
+++ MotorSpecTest/app.c
@@ -5,6 +5,8 @@
 #include "ev3api.h"
 #include "app.h"
 
+#define MSG_BUF_SIZE    (64)
+
 const motor_port_t motor_port = EV3_PORT_A;
 
 int motor_power_output = 0;
@@ -13,10 +15,14 @@ int motor_counts_current = 0;
 int motor_power_output_change = 0;
 int cyc_task_1sec_count = 0;
 int cyc_task_100msec_count = 0;
-const char *logging_file_name = "Log.csv";
+//const char *logging_file_name = "Log.csv";
 
 FILE *logging_file = NULL;
-
+const char *logging_file_name_list[10] = {
+    "Log_001.csv", "Log_002.csv", "Log_003.csv", "Log_004.csv", "Log_005.csv",
+    "Log_006.csv", "Log_007.csv", "Log_008.csv", "Log_009.csv", "Log_010.csv"
+    };
+char msg_buf[MSG_BUF_SIZE];
 /**
  * @biref   Main task of this application.
  *          Initializes parameters, configures device,
@@ -26,26 +32,43 @@ FILE *logging_file = NULL;
  */
 void main_task(intptr_t unused)
 {
+    int index = 0;
+    const char *logging_file_name = NULL;
+
     //Initialize
     ev3_lcd_set_font(EV3_FONT_SMALL);
     ev3_lcd_draw_string("STARTING", 0, 0);
-    ev3_motor_config(motor_port, MEDIUM_MOTOR);
+    ev3_motor_config(motor_port, LARGE_MOTOR);
 
-    logging_file = fopen(logging_file_name, "a");
-    fprintf(logging_file, "CNT,TARGET,CURRENT,COUNTS\n");
+    for (index = 0; index < 10; index++) {
+        logging_file_name = logging_file_name_list[index];
+        logging_file = fopen(logging_file_name, "w");
+        fprintf(logging_file, "CNT,TARGET,CURRENT,COUNTS\n");
 
-    motor_power_output_change = 5;
-    ev3_lcd_draw_string("RUNNING   ", 0, 0);
+        //Reset paraemters.
+        motor_power_output = 0;
+        motor_power_current = 0;
+        motor_counts_current = 0;
+        motor_power_output_change = 5;
 
-    ev3_sta_cyc(CYC_TASK_10MS);
-    ev3_sta_cyc(CYC_TASK_1SEC);
-    tslp_tsk(90000);
+        cyc_task_100msec_count = 0;
+        cyc_task_1sec_count = 0;
 
-    ev3_stp_cyc(CYC_TASK_10MS);
-    ev3_stp_cyc(CYC_TASK_1SEC);
+        ev3_motor_reset_counts(motor_port);
 
-    fclose(logging_file);
-    ev3_lcd_draw_string("FINISHED", 0, 0);
+        sprintf((char *)&msg_buf[0], "RUNNING:%s", logging_file_name);
+        ev3_lcd_draw_string((char *)&msg_buf[0], 0, 0);
+
+        ev3_sta_cyc(CYC_TASK_10MS);
+        ev3_sta_cyc(CYC_TASK_1SEC);
+        tslp_tsk(85000);
+
+        ev3_stp_cyc(CYC_TASK_10MS);
+        ev3_stp_cyc(CYC_TASK_1SEC);
+
+        fclose(logging_file);
+        ev3_lcd_draw_string("FINISHED", 0, 0);
+    }
 }
 
 /**
